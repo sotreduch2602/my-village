3d903c3c70b8d74ed2d982fe772a15f422c43eeb
sotreduch2602 <sotreduch26022001@gmail.com>
2025-12-04 14:31:45 +0700

    add: Header and Sidebar

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,15 +1,5 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Menu } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-
-
-
 export default function App() {
-  return (
-    <>
-    </>
-  );
+  return <></>;
 }
